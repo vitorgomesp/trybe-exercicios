9fcfed9c45a0c8af81f109a9fd11af122ddc73f0
Vitor Gomes <vitorgomes14@hotmail.com>
2023-01-23 16:14:41 -0300

    exercícios de if else e tipos primitivos feitos

---
+++ Fundamentos/3-introducao-a-javascript/javascript-primeiros-passos/scripts2.js
@@ -0,0 +1,7 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = base + height + base + height;
+
+console.log(area);
+console.log(perimeter);
---
+++ Fundamentos/3-introducao-a-javascript/javascript-primeiros-passos/scripts3.js
@@ -0,0 +1,12 @@
+const notaDesafio = 55;
+if (notaDesafio >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+}
+
+else if (notaDesafio < 80 && notaDesafio >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+
+else {
+    console.log("Infelizmente, você reprovou.");
+}
