1b292abcf06dd1d79974f9d4e8033361551c8e79
Vitor Gomes <vitorgomes14@hotmail.com>
2023-01-23 15:37:28 -0300

    Feito o exercício de variáveis

---
+++ Fundamentos/3-introducao-a-javascript/javascript-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+let myName = "Vitor";
+const birthCity = "Aimorés";
+let birthYear = 1996;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+
+let explicacao = "A mensagem de erro aparece porque a variável birthCity foi criada utilizando const, de modo que não pode ser alterada";
+console.log(explicacao);
